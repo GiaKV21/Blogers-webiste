7a01b230f01fffa1233edb297becf9473591097a
CarnageOG <egetashviligiorgi9@gmail.com>
2025-08-17 20:23:21 +0400

    row registation

---
+++ src/main.js
@@ -0,0 +1,17 @@
+function switchTab(tab) {
+  document.querySelectorAll('.tab').forEach(btn => btn.classList.remove('active'));
+  document.querySelectorAll('.form').forEach(form => form.classList.remove('active'));
+
+  if (tab === 'login') {
+    document.getElementById('loginForm').classList.add('active');
+    document.querySelectorAll('.tab')[0].classList.add('active');
+    document.getElementById('formTitle').textContent = "კეთილი იყოს შენი კალამი!";
+  } else if (tab === 'register') {
+    document.getElementById('registerForm').classList.add('active');
+    document.querySelectorAll('.tab')[1].classList.add('active');
+    document.getElementById('formTitle').textContent = "შენი ამბავი იწყება აქ!";
+  } else if (tab === 'reset') {
+    document.getElementById('resetForm').classList.add('active');
+    document.getElementById('formTitle').textContent = "პაროლის აღდგენა";
+  }
+}
