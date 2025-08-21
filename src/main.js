122813efb1b29d1c7e3f774d773c86d4a8e4750b
CarnageOG <egetashviligiorgi9@gmail.com>
2025-08-21 11:29:30 +0400

    some corections in password changer page

--- src/main.js
+++ src/main.js
@@ -32,7 +32,22 @@ function goToStep(step) {
   document.getElementById('step' + step).classList.remove('hidden');
 
   if (step === 2) {
-    const email = document.getElementById('email').value.trim();
+    const email = document.getElementById('youremail').value.trim();
     document.getElementById('showEmail').innerText = email;
   }
+
+  const inputs = document.querySelectorAll(".code");
+  inputs.forEach((input, index) => {
+    input.addEventListener("input", (e) => {
+      if (e.target.value && index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    });
+
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Backspace" && !input.value && index > 0) {
+        inputs[index - 1].focus();
+      }
+    });
+  });
 }
