012aed5c5ac7d74244d4e0f9cc04133de3f878ef
CarnageOG <egetashviligiorgi9@gmail.com>
2025-11-08 17:28:44 +0400

    made user media

--- javascript/user.js
+++ javascript/user.js
@@ -12,3 +12,33 @@ const pages = document.querySelectorAll('.page');
     target.classList.add('active');
   });
 });
+
+const nav = document.querySelector('.nav-user');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+nav.addEventListener('mousedown', e => {
+  isDown = true;
+  nav.classList.add('dragging');
+  startX = e.pageX - nav.offsetLeft;
+  scrollLeft = nav.scrollLeft;
+});
+
+nav.addEventListener('mouseleave', () => {
+  isDown = false;
+  nav.classList.remove('dragging');
+});
+
+nav.addEventListener('mouseup', () => {
+  isDown = false;
+  nav.classList.remove('dragging');
+});
+
+nav.addEventListener('mousemove', e => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - nav.offsetLeft;
+  const walk = (x - startX) * 1;
+  nav.scrollLeft = scrollLeft - walk;
+});
