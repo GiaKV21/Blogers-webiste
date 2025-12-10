8e58510ce676fd1dbc1a122295b29283411b4237
CarnageOG <egetashviligiorgi9@gmail.com>
2025-12-10 19:17:51 +0400

    1

--- javascript/editor.js
+++ javascript/editor.js
@@ -10,8 +10,6 @@ tinymce.init({
       { value: 'First.Name', title: 'First Name' },
       { value: 'Email', title: 'Email' },
     ],
-    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-    uploadcare_public_key: '8de56763523416290ffa',
 });
 
 // MODAL
